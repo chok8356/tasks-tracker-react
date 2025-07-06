23c571c4ad07c0c5c3b09bda9ce773ff6fc2bb44
Nikolai Chemov <nikolai.chemov@retailrocket.io>
2025-07-06 20:30:29 +0300

    eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,45 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import antfu from '@antfu/eslint-config'
+import gitignore from 'eslint-config-flat-gitignore'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
-export default tseslint.config([
-  globalIgnores(['dist']),
+export default antfu(
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    jsonc: true,
+    react: true,
+    stylistic: true,
+    typescript: true,
+  },
+  gitignore(),
+  eslintPluginPrettierRecommended,
+  {
+    files: ['**/*.{js,ts,tsx}'],
+    rules: {
+      'antfu/consistent-list-newline': 'off',
+      curly: ['error', 'all'],
+      'no-console': ['error', { allow: ['warn', 'error', 'info'] }],
+      'perfectionist/sort-enums': ['error', { type: 'alphabetical' }],
+      'perfectionist/sort-imports': ['error', { type: 'alphabetical' }],
+      'perfectionist/sort-interfaces': ['error', { type: 'alphabetical' }],
+      'perfectionist/sort-objects': ['error', { type: 'alphabetical' }],
+      'prettier/prettier': [
+        'error',
+        {
+          arrowParens: 'always',
+          bracketSameLine: true,
+          bracketSpacing: true,
+          endOfLine: 'lf',
+          printWidth: 80,
+          proseWrap: 'always',
+          semi: false,
+          singleAttributePerLine: true,
+          singleQuote: true,
+          tabWidth: 2,
+          trailingComma: 'all',
+        },
+      ],
+      'react-refresh/only-export-components': 'warn',
+      'style/quote-props': ['error', 'as-needed'],
+      'ts/consistent-type-definitions': ['error', 'type'],
     },
   },
-])
+)
